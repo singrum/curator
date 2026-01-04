c23e1345b4a83047c70aec8d859a0eae9472e02e
singrum <miamiq0000@gmail.com>
2026-01-05 00:32:48 +0900

    x

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
