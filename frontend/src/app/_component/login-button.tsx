284a41bdca2b4000b50228f0b4a608a9439b4bf7
singrum <miamiq0000@gmail.com>
2026-01-05 17:52:40 +0900

    fix: update login button to use direct link for Google authentication

--- frontend/src/app/_component/login-button.tsx
+++ frontend/src/app/_component/login-button.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function LoginButton() {
   return (
     <Button variant={"secondary"} asChild size="sm" className="text-xs">
-      <Link href="/auth/login">로그인</Link>
+      <a href={`${process.env.API_BASE_URL}/auth/google`}>로그인</a>
     </Button>
   );
 }
--- frontend/src/app/auth/login/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.redirect(`${process.env.API_BASE_URL}/auth/google`);
-}
