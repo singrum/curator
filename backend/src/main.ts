d3da8a24239f14d7f5378bdc12a00322bcef3459
singrum <miamiq0000@gmail.com>
2026-01-05 17:41:02 +0900

    x

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import { Express } from 'express';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const instance = app.getHttpAdapter().getInstance() as Express;
+  instance.set('trust proxy', 1);
   app.use(cookieParser());
 
   app.useGlobalPipes(
@@ -17,6 +20,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
